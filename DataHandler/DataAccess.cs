a586a01c76a2a26ec63dbf8c5a5e6872ae232c78
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2021-08-02 16:17:14 -0400

    Moved DataHandler to internal project structure, adding Dapper and MySQL dependencies directly to the project.

---
+++ DataHandler/DataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace DataHandler
+{
+    public class DataAccess
+    {
+
+        public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sql, parameters).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Execute(sql, parameters);
+            }
+        }
+
+
+
+    }
+}
