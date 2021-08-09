83e708fd52fbbc7241c1416e0602ab314ccf71fc
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2021-08-09 08:40:27 -0400

    Added functionality to write device elements to database.

--- Controllers/DevicesController.cs
+++ Controllers/DevicesController.cs
@@ -1,4 +1,7 @@
-﻿using AIM_Inventory.Models;
+﻿// Anthony Isensee
+// 8/6/21
+
+using AIM_Inventory.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,7 +16,10 @@ namespace AIM_Inventory.Controllers
     public class DevicesController : Controller
     {
 
-        // Displays a list of devices from a database (db version 0.1)
+        // Summary:
+        //  Displays a list of devices from a database (db version 0.1)
+        // Returns:
+        //  A view with a list of devices.
         public IActionResult Index()
         {
             // Create list.
@@ -37,10 +43,66 @@ namespace AIM_Inventory.Controllers
 
         }
 
-        // Displays a field for creating a new device.
+        // Summary:
+        //  Displays a field for creating a new device.
         public IActionResult Create()
         {
             return View();
         }
+
+        // Summary:
+        //  Stores an entry into the database when the asp-action "create" takes place (trigger from Create view is the "Create" button.
+        // Parameters:
+        //  model: a DeviceModel model that contains device information.
+        // Returns:
+        //  The "Index" view if the model is valid, which should include the new device. If the model is not valid, returns the current view.
+        [HttpPost]  // This [HttpPost] attribute specifies that this is an action that will be performed *after* the Create page is loaded.
+                    // This tag also allows us to have two "Create" methods in this class without errors. (Try removing the tag, I dare you!)
+        public IActionResult Create(DeviceModel model)
+        {
+            // Here we check to make sure that the model follows all the attribute rules of "DeviceModel".
+            // The "Create" view should have kept the user within the parameters, but an extra check is not a bad thing.
+            if (ModelState.IsValid)
+            {
+                // Define the necessary sql string with parameters. (@ symbol is only there so string can wrap multiple lines.)
+                string sql = @"insert into device (id, type, friendly_name, ip_address, serial_number, model_number, mac_address, operating_system, notes, date_purchase, date_retire) 
+                                values (@id, @type, @friendly_name, @ip_address, @serial_number, @model_number, @mac_address, @operating_system, @notes, @date_purchase, @date_retire);";
+                DataAccess _data = new DataAccess();
+                
+                // Use DataHandler's DataAccess class to save data to the specified database.
+                _data.SaveData(
+                    sql,                    // sql string
+                    new { id = model.ID,    // model parameters
+                        type = model.Type, 
+                        friendly_name = model.Friendly_Name, 
+                        ip_address = model.IP_Address,
+                        serial_number = model.Serial_Number,
+                        model_number = model.Model_Number,
+                        mac_address = model.MAC_Address,
+                        operating_system = model.Operating_System,
+                        notes = model.Notes,
+                        date_purchase = model.Date_Purchase,
+                        date_retire = model.Date_Retire },   
+                    "Server=127.0.0.1;Port=3306;database=inventory_local_test;user id=local_user;password=password;");  // connection string
+
+                // Returns the "Index" view. The new device should be populated to the list.
+                // Note that if we did not specify the "Index" view, the "Create" view would be returned instead.
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Delete()
+        {
+            return View("Index");
+        }
+
     }
 }
--- Models/DeviceModel.cs
+++ Models/DeviceModel.cs
@@ -22,33 +22,41 @@ namespace AIM_Inventory.Models
 
         // The type of device (for instance: server, laptop, etc.)
         [Display(Name = "Device Type")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters.")]
         public string Type { get; set; }
 
         // Friendly Name
         [Display(Name = "Device Name")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters. Supercalifragilisticexpialidocious even fits, but your name doesn't?")]
         public string Friendly_Name { get; set; }
 
         // IP Address
         [Display(Name = "IP Address")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters. What are you using, IPv128?")]
         public string IP_Address { get; set; }
 
         // Serial Number
         [Display(Name = "Serial Number")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters. No exceptions.")]
         public string Serial_Number { get; set; }
 
         // Model Number
         [Display(Name = "Model Number")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters. I really hope you don't run into any 128 character model numbers!")]
         public string Model_Number { get; set; }
 
         // MAC Address
         [Display(Name = "MAC Address")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters, though you really shouldn't need that many.")]
         public string MAC_Address { get; set; }
 
         // Operating System
         [Display(Name = "Operating System")]
+        [StringLength(128, ErrorMessage = "Limit of 128 characters (seriously, how do you need 128 characters?).")]
         public string Operating_System { get; set; }
 
         // Notes
+        [StringLength(10000, ErrorMessage = "Limit of 10,000 characters. Is that really not enough?")]
         [Display(Name = "Notes")]
         public string Notes { get; set; }
 
